a14da6937d0ea5ad0893391456a6419c455b9812
Preet Lalli <35373683+preetl@users.noreply.github.com>
2023-10-20 11:46:00 +0100

    Update lora_download.py for llama 2 7B
    Signed-off-by: Preet Lalli <35373683+preetl@users.noreply.github.com>

--- sample-submissions/lit-gpt/lora_download.py
+++ sample-submissions/lit-gpt/lora_download.py
@@ -1,7 +1,7 @@
 from huggingface_hub import snapshot_download
 
 snapshot_download(
-    repo_id="preetlalli/llama-3b-lora",
+    repo_id="preetlalli/llama-2-7B",
     token="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",
-    local_dir="open-llama-3b",
+    local_dir="llama-2-7B",
 )
