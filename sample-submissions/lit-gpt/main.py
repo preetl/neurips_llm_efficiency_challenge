51239eabd23a30d11725a59f0f18f8e649a589de
Preet Lalli <35373683+preetl@users.noreply.github.com>
2023-10-20 11:46:58 +0100

    Update main.py for llama 2 7B
    Signed-off-by: Preet Lalli <35373683+preetl@users.noreply.github.com>

--- sample-submissions/lit-gpt/main.py
+++ sample-submissions/lit-gpt/main.py
@@ -39,7 +39,7 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 quantize = "bnb.nf4-dq"  # 4-bit NormalFloat with Double-Quantization (see QLoRA paper)
-checkpoint_dir = Path("checkpoints/openlm-research/open_llama_3b")
+checkpoint_dir = Path("checkpoints/meta-llama/Llama-2-7b-hf")
 precision = "bf16-true"  # weights and data in bfloat16 precision
 
 fabric = L.Fabric(devices=1, accelerator="cuda", precision=precision)
