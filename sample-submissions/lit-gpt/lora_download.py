b60c8853b992b55c261129fccbf076a889bcaf8f
Preet Lalli <35373683+preetl@users.noreply.github.com>
2023-10-23 15:42:16 +0100

    Update lora_download.py for mistral 7B
    Signed-off-by: Preet Lalli <35373683+preetl@users.noreply.github.com>

--- sample-submissions/lit-gpt/lora_download.py
+++ sample-submissions/lit-gpt/lora_download.py
@@ -1,7 +1,7 @@
 from huggingface_hub import snapshot_download
 
 snapshot_download(
-    repo_id="preetlalli/llama-2-7B",
+    repo_id="preetlalli/mistral-7B",
     token="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",
-    local_dir="llama-2-7B",
+    local_dir="mistral-7B",
 )
